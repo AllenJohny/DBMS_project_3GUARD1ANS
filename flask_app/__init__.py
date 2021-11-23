e7a1bffa173dbd98b17392e4f0fbacc5ed696541
Allen Chacko Johny <64266308+Allenjohny@users.noreply.github.com>
2021-11-23 20:05:15 +0530

    initial files

---
+++ flask_app/__init__.py
@@ -0,0 +1,12 @@
+from flask import Flask
+from flask_pymongo import PyMongo
+
+
+app=Flask(__name__)
+
+app.config["MONGO_URI"]="mongodb+srv://user1:uJEo0dIWpC6s4wc0@dbmsproject.hqrwo.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+
+mongodb_client=PyMongo(app)
+db=mongodb_client.db
+
+from flask_app import route
