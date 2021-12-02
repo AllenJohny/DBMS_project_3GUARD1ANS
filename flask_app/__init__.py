538790d2e22fe58f20180f7539380e52bae042d5
Allen Chacko Johny <64266308+Allenjohny@users.noreply.github.com>
2021-12-02 18:22:41 +0530

    update

--- flask_app/__init__.py
+++ flask_app/__init__.py
@@ -3,6 +3,7 @@ from flask_pymongo import PyMongo
 
 
 app=Flask(__name__)
+app.secret_key = "allen"
 
 app.config["MONGO_URI"]="mongodb+srv://user1:uJEo0dIWpC6s4wc0@dbmsproject.hqrwo.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
 
